4d4acb772b02910df41180b338a28c2c6b5e7fb3
Suman-Adhikary <mynamesuman007@gmail.com>
2022-11-17 16:31:19 +0530

    Learning flask

---
+++ Higher-Lower.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def Game():
+    return "<h1> Guess a number between 0 and 9 </h1>" \
+            "<img src = 'https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' width=400, height=400>"
+
+computer_select = random.randint(0,9)
+
+@app.route("/<int:num>")
+def guess(num):
+    if(computer_select == num):
+        return "<h1> You found me! </h1>" \
+                "<img src = 'https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif' width=400, height=400>"
+    if (computer_select > num):
+        return "<h1> Too low, try again! </h1>" \
+               "<img src = 'https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif' width=400, height=400>"
+    if (computer_select < num):
+        return "<h1> Too high, try again! </h1>" \
+               "<img src = 'https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif' width=400, height=400>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
